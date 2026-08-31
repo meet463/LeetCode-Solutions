f4359529d78e4d522cc99fc38aa4d09f3c8252cd
Meet <meetnagar1810@gmail.com>
2026-08-31 12:13:55 +0530

    Time: 0 ms (100%), Space: 43.4 MB (43.04%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] plusOne(int[] arr) {
+        for(int i = arr.length - 1; i >= 0; i--){
+            if (arr[i] < 9) {
+            arr[i]++;
+            return arr;
+        } 
+        arr[i] = 0;
+    }
+    int[] newArr = new int[arr.length + 1] ;
+    newArr[0] = 1;
+    return newArr;
+  }
+}
